c540f28326122ecc78d64976193161bb797117e5
D3vJoy <168494262+J01C3@users.noreply.github.com>
2026-06-18 07:52:40 -0300

    Corrige build e integração com Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDkQoXM7-E081XYfBW2JwkLXl50wCEvOvw",
+    authDomain: "avaliacao-mobile-9ca2a.firebaseapp.com",
+    projectId: "avaliacao-mobile-9ca2a",
+    storageBucket: "avaliacao-mobile-9ca2a.firebasestorage.app",
+    messagingSenderId: "1043451433493",
+    appId: "1:1043451433493:web:b66e35c75a6d244422b41c",
+    measurementId: "G-SL708NK3CX"
+  }
 };
